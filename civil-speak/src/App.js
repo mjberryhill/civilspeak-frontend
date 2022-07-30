77eb19868f44afa5491020bb6bda5676fdee5696
Alexander Berryhill <amsb99@gmail.com>
2022-07-30 16:46:18 -0600

    start

---
+++ civil-speak/src/App.js
@@ -0,0 +1,16 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import './App.css';
+
+import Landing from './pages/Landing';
+
+function App() {
+    return (
+      <Router>
+        <Routes>
+          <Route path='/' exact element={<Landing />} />
+        </Routes>
+      </Router>
+    );
+  }
+  
+export default App;
